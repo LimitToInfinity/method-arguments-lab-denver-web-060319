c20bde96bc7a7841d2ed04f90e5c71138eb12008
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-26 22:36:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-def Introduction(name)
+def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def Introduction(name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
